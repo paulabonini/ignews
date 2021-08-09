60195e4a5f5b6a6a9ae7c6d0f38b06270eae724e
Paula Bonini <paula_almeidabonini@hotmail.com>
2021-08-09 18:14:13 -0300

    fixed bugs

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,9 @@
-import { query as q } from 'faunadb';
+import { query as q } from "faunadb";
 
-import NextAuth from 'next-auth';
-import { session } from 'next-auth/client';
-import Providers from 'next-auth/providers';
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
-import {fauna} from '../../../services/fauna';
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   //Configure one or more auth providers
@@ -12,7 +11,7 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
     // ... add providers here
   ],
@@ -20,76 +19,62 @@ export default NextAuth({
     //sintaxe FQL fauna query language
 
     async session(session) {
-     try {
-      const userActiveSubscription = await fauna.query(
-        q.Get(
-          q.Intersection( [
-            q.Match(
-              q.Index('subscription_by_user_ref'),
-              q.Select(
-                'ref',
-                q.Get(
-                  q.Match(
-                    q.Index('user_by_email'),
-                    q.Casefold(session.user.email)
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
                   )
-                ) 
-              )
-            ),
-            q.Match(
-              q.Index('subscription_by_status'),
-              "active"
-            )
-          ])
-        )
-      )
-      
-      return {
-        ...session, 
-        activeSubscription: userActiveSubscription
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
       }
-     } catch {
-      return {
-        ...session,
-        activeSubscription: null,
-      }
-     }
     },
 
     async signIn(user, account, profile) {
-      const { email } = user
+      const { email } = user;
 
       try {
         await fauna.query(
-         q.If(
-           q.Not(
-             q.Exists(
-               q.Match(
-                 q.Index('user_by_email'),
-                 q.Casefold(user.email)
-               )
-             )
-           ),
-           q.Create(
-            q.Collection('users'),
-            {data: { email }}
-          ),
-          q.Get(
-            q.Match(
-              q.Index('user_by_email'),
-              q.Casefold(user.email)
-            )
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
-         )
-        )
+        );
 
-        return true
+        return true;
       } catch {
-        return false
+        return false;
       }
     },
-  }
+  },
 
   // A database is optional, but required to persist accounts in database
   // database: process.env.DATABASE_URL
-})
+});
